24170e1f65e86c6e5298ab5519b88bcefe242d16
Ethan Bourguigneau <ethanbourguigneau@gmail.com>
2023-08-05 00:08:38 +0200

    Init readme project
    Init main files

---
+++ index.js
@@ -0,0 +1,47 @@
+const readme = require("./readme.js");
+const fs = require("fs");
+require("dotenv").config();
+
+// Global variables
+const DEFAULT_BIO = '';
+const DEFAULT_BIO1 = '';
+const DEFAULT_BIO2 = '';
+
+function generateNewREADME() {
+  // TODO: Generate new READMEs
+}
+
+function updateREADME(newREADME) {
+  fs.writeFileSync("./README2.md", newREADME);
+}
+
+// Function to main 
+async function main() {
+  const newREADME = generateNewREADME();
+  console.log(newREADME);
+  updateREADME(newREADME);
+}
+main();
+
+// Implementation IA API IN PROGRESS
+// const { Configuration, OpenAIApi } = require("openai");
+// const configuration = new Configuration({
+//   organization: "org-KvjXVgEc9gQ8sMjRCRac7Kp2",
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// const openai = new OpenAIApi(configuration);
+
+// openai
+//   .createCompletion({
+//     model: "text-davinci-003",
+//     prompt: "Say this is a test",
+//     max_tokens: 7,
+//     temperature: 0,
+//   })
+//   .then((response) => {
+//     console.log(response.data);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
---
+++ readme.js
@@ -0,0 +1,19 @@
+module.exports = `
+# 💫 About Me:
+Hi 👋, I'm Ethan Bourguigneau and I'm a junior web developer from France. I aspire to become a good fullstack developer one day.<br><br>🌱 I’m currently learning on the implementation of AI in an application.<br>📫 How to reach me ethanbourguigneau@gmail.com<br>
+
+
+## 🌐 Socials:
+[![Discord](https://img.shields.io/badge/Discord-%237289DA.svg?logo=discord&logoColor=white)](https://discord.gg/ethan_hdb#9057) [![Instagram](https://img.shields.io/badge/Instagram-%23E4405F.svg?logo=Instagram&logoColor=white)](https://instagram.com/ethan_hdb) [![LinkedIn](https://img.shields.io/badge/LinkedIn-%230077B5.svg?logo=linkedin&logoColor=white)](https://linkedin.com/in/ethan-bourguigneau-441197263) 
+
+# 💻 Tech Stack:
+![PHP](https://img.shields.io/badge/php-%23777BB4.svg?style=flat-square&logo=php&logoColor=white) ![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=flat-square&logo=express&logoColor=%2361DAFB) ![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=flat-square&logo=node.js&logoColor=white) ![Symfony](https://img.shields.io/badge/symfony-%23000000.svg?style=flat-square&logo=symfony&logoColor=white) ![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=flat-square&logo=tailwind-css&logoColor=white) ![Vue.js](https://img.shields.io/badge/vuejs-%2335495e.svg?style=flat-square&logo=vuedotjs&logoColor=%234FC08D) ![Postgres](https://img.shields.io/badge/postgres-%23316192.svg?style=flat-square&logo=postgresql&logoColor=white) ![MySQL](https://img.shields.io/badge/mysql-%2300f.svg?style=flat-square&logo=mysql&logoColor=white) 	![Figma](https://img.shields.io/badge/figma-%23F24E1E.svg?style=flat-square&logo=figma&logoColor=white) ![Postman](https://img.shields.io/badge/Postman-FF6C37?style=flat-square&logo=postman&logoColor=white) ![Notion](https://img.shields.io/badge/Notion-%23000000.svg?style=flat-square&logo=notion&logoColor=white) ![Jira](https://img.shields.io/badge/jira-%230A0FFF.svg?style=flat-square&logo=jira&logoColor=white) ![Docker](https://img.shields.io/badge/docker-%230db7ed.svg?style=flat-square&logo=docker&logoColor=white) ![Trello](https://img.shields.io/badge/Trello-%23026AA7.svg?style=flat-square&logo=Trello&logoColor=white)
+# 📊 GitHub Stats:
+![](https://github-readme-stats.vercel.app/api?username=A5hura666&theme=vue-dark&hide_border=false&include_all_commits=false&count_private=true)<br/>
+![](https://github-readme-streak-stats.herokuapp.com/?user=A5hura666&theme=vue-dark&hide_border=false)<br/>
+![](https://github-readme-stats.vercel.app/api/top-langs/?username=A5hura666&theme=vue-dark&hide_border=false&include_all_commits=false&count_private=true&layout=compact)
+
+---
+[![](https://visitcount.itsvg.in/api?id=A5hura666&icon=9&color=0)](https://visitcount.itsvg.in)
+
+`;
